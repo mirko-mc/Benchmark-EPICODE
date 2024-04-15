b39e98af8d321a513f3bdc226251a2f78ce8e6c5
DarkSide Labs <8547740+DarkSideLabs@users.noreply.github.com>
2024-04-15 03:28:30 +0200

    deleted timer files

--- assets/js/timer.js
+++
@@ -1,9 +0,0 @@
-const anello = document.querySelector('.anello');
-let counter = 0;
-const anelloInterval = setInterval(() => {
-  anello.style.background = `conic-gradient(grey ${counter}%, aqua 0%) border-box`;
-  counter += 3.3;
-  if (counter < 0) {
-    clearInterval(anelloInterval);
-  }
-}, 1000);
